1310a613af43f960f4e31206714735bd1779c30f
Ravi Puppala <rpuppala@opentext.com>
2022-10-28 19:34:09 +0530

    navigation links functionality and bit styling

--- src/components/body/left.navigation/left.navigation.js
+++ src/components/body/left.navigation/left.navigation.js
@@ -1,45 +1,46 @@
-import React, { useState, useRef, useContext, useEffect } from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { Link , NavLink} from "react-router-dom";
 import ContextComponent from "../../app.context";
 import Utils from "../../utils";
 
 const LeftNavigation = () => {
   let { userData } = useContext(ContextComponent);
 
-  let [stateNavKeys, setStateNavKeys] = useState([]);
-  let [stateNavValues, setStateNavValues] = useState({});
-
   let navLinkKeys = Utils.NAV_ITEMS_KEYS;
   let navLinkValues = Utils.NAV_ITEMS_VALUES;
 
-  let updatedNavLinks = {}; 
-  let updatedNavKeys = [];
-  let newNavLinkValues = {};
+  let tempFilteredNavLinkKeys = [];
+  let [filteredNavLinkKeys, setFilteredNavLinkKeys] = useState([]);
+
+  console.log(this);
 
   useEffect(() => {
-    navLinkKeys.forEach((a) => {
-      newNavLinkValues[a] = navLinkValues[a];
-      let enabled = newNavLinkValues[a].enabled || false;
-      !enabled && userData.isAdmin && (newNavLinkValues[a].enabled = true);
-      enabled && (updatedNavLinks[a] = newNavLinkValues[a]);
-      enabled && updatedNavKeys.push(a);
-    });
+    if (Object.keys(userData).length) {
+      console.log(this);
+      let isAdmin = userData.isAdmin;
+      navLinkKeys.forEach((navLinkKey) => {
+        if (isAdmin || navLinkValues[navLinkKey].enabled) {
+          tempFilteredNavLinkKeys.push(navLinkKey);
+        }
+      });
+      setFilteredNavLinkKeys(tempFilteredNavLinkKeys);
+    }
+  }, [userData]);
 
-    setStateNavKeys(updatedNavKeys);
-    setStateNavValues(updatedNavLinks);
-  }, []);
-
-  return (
-    <div className="dbc-left-navigation-wrapper">
-      {stateNavKeys.map((navKey, index) => (
-        <div className="dbc-navmenu-item" key={index}>
-          <Link to={stateNavValues[navKey].url}>
-            {stateNavValues[navKey].title}
-          </Link>
-        </div>
-      ))}
-    </div>
-  );
+return (
+  <div className="dbc-left-navigation-wrapper">
+    {filteredNavLinkKeys.map((navKey, index) => (
+      <div className="dbc-navmenu-item" key={index}>
+        <NavLink
+          className={({ isActive }) => (isActive ? 'dbc-navmenu-active' : 'dbc-navmenu-inactive')}
+          to={navLinkValues[navKey].url}
+        >
+          {navLinkValues[navKey].title}
+        </NavLink>
+      </div>
+    ))}
+  </div>
+);
 };
 
 export default LeftNavigation;
--- src/components/controls/dbc.user.profile.menu.js
+++ src/components/controls/dbc.user.profile.menu.js
@@ -37,6 +37,7 @@ const DBCUserProfileMenu = () => {
     } else {
       //setUserImage(defaultUserImage);
     }
+    console.log(userData.firstName + " " + userData.lastName);
   }, [userData]);
 
   //setTimeout(function () {
