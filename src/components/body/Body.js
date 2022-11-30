2ab5323136d0921c94ccb1a759fecf9028a64ae3
Ravi Puppala <ravi.999@gmail.com>
2022-12-01 02:51:28 +0530

    moved obtaining addresse bit advance to work with ext card page

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -4,73 +4,11 @@ import ContentArea from "./content.area/ContentArea";
 import ContextComponent from "../AppContext";
 import Utils from "../Utils";
 
-const Body = () => {
-  let [canRender, setCanRender] = useState(true);
-  let { addrCtxData, setAddrCtxData } = useContext(ContextComponent);
-
-  useEffect(() => {
-    const success = (res) => {
-      let data = res.data;
-      let tempAddrIds = [];
-      let tempAddrNames = [];
-      let tempAddrValues = [];
-      let tempCityValues = [];
-      let tempCountryValues = [];
-      let tempZipValues = [];
-      let tempFullAddrValues = [];
-      let tempAddrNumbers = [];
-      data.map((addr, index) => {
-        //debugger;
-        tempAddrIds.push(addr.id);
-        tempAddrNames.push(addr.name);
-
-        let address = addr?.address || "";
-        let city = addr?.city || "";
-        let country = addr?.country || "";
-        let zip = addr?.zip || "";
-
-        tempAddrValues.push(address);
-        tempCityValues.push(city);
-        tempCountryValues.push(country);
-        tempZipValues.push(zip);
-
-        tempFullAddrValues.push(
-          address + " " + city + " " + country + " " + zip
-        );
-        tempAddrNumbers.push(addr.contact);
-      });
-
-      let addrObj = {
-        ids: tempAddrIds,
-        addressNames: tempAddrNames,
-        addresses: tempAddrValues,
-        cities: tempCityValues,
-        countries: tempCountryValues,
-        fullAddresses: tempFullAddrValues,
-        numbers: tempAddrNumbers,
-      };
-      setAddrCtxData(addrObj);
-    };
-    const fail = (err) => {
-      console.log(err);
-    };
-
-    try {
-      Utils.getAddresses().then(success, fail);
-    } catch (e) {
-      console.log(e);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (!Utils.isObjectEmpty(addrCtxData)) {
-      setCanRender(true);
-    }
-  }, [addrCtxData]);
+const Body = () => { 
 
   return (
     <>
-      {canRender && (
+      
         <div className="indi-body-wrapper container ">
           <div className="indi-body-row h-100">
             <div className="row h-100">
@@ -83,7 +21,7 @@ const Body = () => {
             </div>
           </div>
         </div>
-      )}
+     
     </>
   );
 };
